e08950abe172444d785972ab01cc793706e15845
theqoobee <qoobeethegreat@gmail.com>
2020-03-15 12:19:11 +0100

    The navbar is now passed at page load

--- app.js
+++ app.js
@@ -22,27 +22,63 @@ app.use(expressSession( {
 }));
 
 const title = 'opusteno.org';
+const navbarTemplate = "<nav class=\"navbar navbar-expand-lg bg-secondary text-uppercase fixed-top\" id=\"mainNav\">\n" +
+    "    <div class=\"container\">\n" +
+    "      <a class=\"navbar-brand js-scroll-trigger\" href=\"#page-top\"><%= title %></a>\n" +
+    "      <button class=\"navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-secondary text-white rounded\"\n" +
+    "        type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarResponsive\" aria-controls=\"navbarResponsive\"\n" +
+    "        aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n" +
+    "        Menu\n" +
+    "        <i class=\"fas fa-bars\"></i>\n" +
+    "      </button>\n" +
+    "      <div class=\"collapse navbar-collapse\" id=\"navbarResponsive\">\n" +
+    "        <ul class=\"navbar-nav ml-auto\">\n" +
+    "          <li class=\"nav-item mx-0 mx-lg-1\">\n" +
+    "            <a class=\"nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger\" href=\"index\">Pocetna</a>\n" +
+    "          </li>\n" +
+    "          <li class=\"nav-item mx-0 mx-lg-1\">\n" +
+    "            <a class=\"nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger\" href=\"about\">O Projektu</a>\n" +
+    "          </li>\n" +
+    "        </ul>\n" +
+    "      </div>\n" +
+    "    </div>\n" +
+    "  </nav>";
+function processNavbars() {
+  return navbarTemplate.replace('<%= title %>', title);
+}
+var navbar = processNavbars();
+var elements = {
+  "title": title,
+  "navbar": navbar
+};
 
 app.get('/', function(req, res, next) {
-  res.render("index.ejs", {title: title});
+  res.render("index.ejs", elements);
 });
 
 app.get('/form', function(req, res, next) {
-  res.render("form.ejs", {title: title});
+  res.render("form.ejs", elements);
 });
 
 app.get('/about', function(req, res, next) {
-  res.render("about.ejs", {title: title});
+  res.render("about.ejs", elements);
 });
 
 app.get('/confirmation', function(req, res, next) {
-  res.render("confirmation.ejs", {email: "none", title: title});
+  let elementsWithEmail = elements;
+  elementsWithEmail.email = "none"; // Just appends the email attribute to the elements
+
+  res.render("confirmation.ejs", elementsWithEmail);
 });
 
 app.post('/confirmation', function(req, res, next) {
   let email = req.body.email;
   req.session.email = email;
-  res.render("confirmation.ejs", {email: email, title: title});
+
+  let elementsWithEmail = elements;
+  elementsWithEmail.email = email; // Just appends the email attribute to the elements
+
+  res.render("confirmation.ejs", elementsWithEmail);
   res.end();
 });
 
