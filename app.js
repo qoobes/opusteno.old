7dadca97768f3c9c122217e9d23fae28709c9c37
theqoobee <qoobeethegreat@gmail.com>
2020-03-16 21:37:09 +0100

    Implemented dynamic head loading, and a couple other things i do not remember

--- app.js
+++ app.js
@@ -22,34 +22,57 @@ app.use(expressSession( {
 }));
 
 const title = 'opusteno.org';
-const navbarTemplate = "<nav class=\"navbar navbar-expand-lg bg-secondary text-uppercase fixed-top\" id=\"mainNav\">\n" +
-    "    <div class=\"container\">\n" +
-    "      <a class=\"navbar-brand js-scroll-trigger\" href=\"#page-top\"><%= title %></a>\n" +
-    "      <button class=\"navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-secondary text-white rounded\"\n" +
-    "        type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarResponsive\" aria-controls=\"navbarResponsive\"\n" +
-    "        aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n" +
-    "        Menu\n" +
-    "        <i class=\"fas fa-bars\"></i>\n" +
-    "      </button>\n" +
-    "      <div class=\"collapse navbar-collapse\" id=\"navbarResponsive\">\n" +
-    "        <ul class=\"navbar-nav ml-auto\">\n" +
-    "          <li class=\"nav-item mx-0 mx-lg-1\">\n" +
-    "            <a class=\"nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger\" href=\"index\">Pocetna</a>\n" +
-    "          </li>\n" +
-    "          <li class=\"nav-item mx-0 mx-lg-1\">\n" +
-    "            <a class=\"nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger\" href=\"about\">O Projektu</a>\n" +
-    "          </li>\n" +
-    "        </ul>\n" +
-    "      </div>\n" +
-    "    </div>\n" +
-    "  </nav>";
-function processNavbars() {
-  return navbarTemplate.replace('<%= title %>', title);
-}
-var navbar = processNavbars();
+const navbarTemplate = `<nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">${title}</a>
+      <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-secondary text-white rounded"
+        type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive"
+        aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item mx-0 mx-lg-1">
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="index">Pocetna</a>
+          </li>
+          <li class="nav-item mx-0 mx-lg-1">
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="about">O Projektu</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>`;
+
+  var headTemplate = `<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="qoobes">
+    <meta name="description" content="A website to improve communication between students and schools">
+    <meta property="og:title" content="Opusteno - talking solves problems">
+    <meta name="theme-color" content="#6dc75a">
+    <meta name="twitter:description" content="Leading student-school communication forward">
+    <meta name="twitter:card" content="summary_medium_image">
+    <meta name="twitter:image" content="/img/handshake.png">
+    <meta name="twitter:author" content="qoobes#0904">
+  
+    <title> ${title} </title>
+    <link rel="icon" href="img/handshake.png">
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet"
+          type="text/css">
+    <link rel="stylesheet" href="css/base.css">
+    <link rel="stylesheet" href="css/addition.css">
+    </head>`;
+
+
+var navbar = processElements(navbarTemplate);
+var head = processElements(headTemplate);
 var elements = {
   title: title,
-  navbar: navbar
+  navbar: navbar,
+  head: head
 };
 
 app.get('/', function(req, res, next) {
